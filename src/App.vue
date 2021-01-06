0ee6ea21c5be22de1105c02338999eed40504d94
Lucille <lucille.gillard@macroprog.com>
2021-01-06 20:09:37 +0100

    Add: header (without style)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <b-navbar toggleable="sm" type="light" variant="light">
+        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+        <b-navbar-brand>Tutoriel</b-navbar-brand>
+
+        <b-collapse id="nav-text-collapse" is-nav>
+          <b-navbar-nav>
+            <!-- TODO: use variable for boardgames box value -->
+            <b-nav-text>Boite de jeu : Demo adventures</b-nav-text>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </header>
+    <main>
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </main>
+    <footer>TODO</footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'App', components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+#app
+{
+  color:                   #2c3e50;
+  font-family:             Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing:  antialiased;
+  margin-top:              60px;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align:              center;
 }
 </style>
