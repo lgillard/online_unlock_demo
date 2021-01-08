4f0ac6e02a611b796e4506b60a5e1b1c9c462a75
Lucille <lucille.gillard@macroprog.com>
2021-01-08 14:54:36 +0100

    Add: searchCard navlink

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,9 +6,18 @@
       <b-navbar-brand>Tutoriel</b-navbar-brand>
 
       <b-collapse id="nav-text-collapse" is-nav>
+        <!-- Left part -->
         <b-navbar-nav>
           <!-- TODO: use variable for boardgames box value -->
           <b-nav-text>Boite de jeu : Demo adventures</b-nav-text>
+        </b-navbar-nav>
+
+        <!-- Right part -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item>
+            <b-icon-search class="mr-3"/>
+            <div class="d-inline">Rechercher une carte</div>
+          </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
