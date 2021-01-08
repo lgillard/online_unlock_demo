50114510464af3e6a93d999635d3787857e45fd3
Lucille <lucille.gillard@macroprog.com>
2021-01-08 12:21:58 +0100

    Add: move card in board
    refacto and improve required

--- src/components/game/Card.vue
+++ src/components/game/Card.vue
@@ -1,11 +1,14 @@
 <template>
-  <img :src="isBack ? backImgSrc : imgSrc" alt="Start card" class="card-size grab"/>
+  <img :id="name" :src="isBack ? backImgSrc : imgSrc" :style="getXYStyle" alt="Start card" class="card-size grab" @click="returnCard()"/>
 </template>
 
 <script>
 export default {
   name:        'Card', props: {
-    scenario: { default: 'demo' }, name: { default: 'start' }, isBack: { default: true },
+    returnAllowed: { default: true }, x: { default: 100 }, y: { default: 100 }, scenario: { default: 'demo' }, name: { default: 'start' },
+  }, data()
+  {
+    return { isBack: true };
   }, computed: {
     backImgSrc()
     {
@@ -13,16 +16,28 @@ export default {
     }, imgSrc()
     {
       return this._getImgUrl(this.name);
+    }, getXYStyle()
+    {
+      return 'left: ' + this.x + 'px;' + 'top: ' + this.y + 'px;';
     },
   }, methods:  {
     _getImgUrl(imgName)
     {
       return '/assets/gameList/' + this.scenario + '/' + imgName + '.JPG';
+    }, returnCard()
+    {
+      if (this.returnAllowed)
+      {
+        this.isBack = !this.isBack;
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-
+img
+{
+  position: absolute;
+}
 </style>
