2967dd5b1ff49b8bdd68e3c93608e73926e4e80a
Lucille <lucille.gillard@macroprog.com>
2021-01-12 10:51:51 +0100

    Rmv: useless cancel button

--- front/src/components/searchCards/SearchCardsModal.vue
+++ front/src/components/searchCards/SearchCardsModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-modal id="search-cards-modal" size="xl">
+  <b-modal id="search-cards-modal" ok-only size="xl">
     <h2>Rechercher une carte</h2>
     <Pick :cards="cards" :socket="socket"/>
   </b-modal>
