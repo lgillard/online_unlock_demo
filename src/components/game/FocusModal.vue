dfc3a58cc63f95c1a5fcb9add86e7b6b14df24a8
Lucille <lucille.gillard@macroprog.com>
2021-03-02 18:38:23 +0100

    Upd: button style

--- src/components/game/FocusModal.vue
+++ src/components/game/FocusModal.vue
@@ -1,37 +1,55 @@
 <template>
   <b-modal :id="'focus-card-' + card.name + '-modal'" hide-footer size="lg">
     <div class="text-center">
-      <img :id="'focus-'+card.name" :src="src" :style="getRotation" alt="Card" class="m-0"/>
-      <div class="h2 mb-2 d-flex justify-content-around mt-5">
-        <div v-b-tooltip.hover
-             class="pointer icon"
-             title="Tourner la carte à gauche"
-             @mousedown="rotate('right')"
-             @mouseup="stopRotate"
-             @touchend="stopRotate"
-             @touchstart="rotate('right')">
-          <b-icon-arrow-counterclockwise/>
-        </div>
-        <div v-b-tooltip.hover
-             class="pointer icon"
-             title="Tourner la carte à droite"
-             @mousedown="rotate('left')"
-             @mouseup="stopRotate"
-             @touchend="stopRotate"
-             @touchstart="rotate('left')">
-          <b-icon-arrow-clockwise/>
-        </div>
+      <div class="d-flex justify-content-around mt-5">
+        <b-button v-b-tooltip.hover
+                  class="icon"
+                  title="Tourner la carte à gauche"
+                  variant="light"
+                  @mousedown="rotate('right')"
+                  @mouseout="stopRotate"
+                  @mouseup="stopRotate"
+                  @touchend="stopRotate"
+                  @touchout="stopRotate"
+                  @touchstart="rotate('right')">
+          <div class="h2 mb-0">
+            <b-icon-arrow-counterclockwise/>
+          </div>
+        </b-button>
+        <b-button v-b-tooltip.hover
+                  class="icon"
+                  title="Tourner la carte à droite"
+                  variant="light"
+                  @mousedown="rotate('left')"
+                  @mouseout="stopRotate"
+                  @mouseup="stopRotate"
+                  @touchend="stopRotate"
+                  @touchout="stopRotate"
+                  @touchstart="rotate('left')">
+          <div class="h2 mb-0">
+            <b-icon-arrow-clockwise/>
+          </div>
+        </b-button>
       </div>
-      <div class="h2 mb-2 d-flex justify-content-around mt-5">
-        <div v-b-tooltip.hover class="pointer icon" title="Retourner la carte" @click="() => {this.$emit('returnCard')}">
-          <b-icon-front/>
-        </div>
-        <div v-b-tooltip.hover class="pointer icon" title="Défausser la carte" @click="() => {this.$emit('discard')}">
-          <b-icon-trash/>
-        </div>
-        <div v-b-tooltip.hover class="pointer icon" title="Reposer la carte dans la pioche" @click="() => {this.$emit('backToPick')}">
-          <b-icon-arrow-bar-up/>
-        </div>
+
+      <img :id="'focus-'+card.name" :src="src" :style="getRotation" alt="Card" class="m-0"/>
+
+      <div class="d-flex justify-content-around mt-5">
+        <b-button v-b-tooltip.hover class="icon" title="Retourner la carte" variant="light" @click="() => {this.$emit('returnCard')}">
+          <div class="h2 mb-0">
+            <b-icon-front/>
+          </div>
+        </b-button>
+        <b-button v-b-tooltip.hover class="icon" title="Défausser la carte" variant="light" @click="() => {this.$emit('discard')}">
+          <div class="h2 mb-0">
+            <b-icon-trash/>
+          </div>
+        </b-button>
+        <b-button v-b-tooltip.hover class="icon" title="Reposer la carte dans la pioche" variant="light" @click="() => {this.$emit('backToPick')}">
+          <div class="h2 mb-0">
+            <b-icon-arrow-bar-up/>
+          </div>
+        </b-button>
       </div>
     </div>
   </b-modal>
